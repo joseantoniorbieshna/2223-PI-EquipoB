b0b5294db3f7b4e78bf02465f4090881e323910b
joseantoniorbieshna <117735985+joseantoniorbieshna@users.noreply.github.com>
2023-05-17 20:24:27 +0200

    Distribution css home

--- frontend/react-web/src/components/MusicContainer/MusicContainer.jsx
+++ frontend/react-web/src/components/MusicContainer/MusicContainer.jsx
@@ -11,8 +11,28 @@ export default function MusicContainer(){
                 <li>...</li>
             </ol>
         </div>
-        <div className="music_container">
-            aaaa
+        <div className="music_manage_container">
+            <div className="music_container" >
+                <div style={{backgroundColor: 'green', height: '10%', width: "50%"}}>sadf</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>sadf</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>daf</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>sa</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>sadf</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>sadf</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>asfd</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>sa</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>a</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>asd</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>sadf</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>dsaf</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>sadf</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>safd</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>asfdsa</div>
+                <div style={{backgroundColor: 'green', height: '10%', width: "100%"}}>asfdas</div>
+                
+
+            </div>
+            <div className="reproductor"></div>
         </div>
     </main>
     )
